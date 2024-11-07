2a02b71c498aab7029e39d665d4aac535a3b95a1
den-g254 <denzelgitonga58@gmail.com>
2024-11-07 21:57:08 +0300

    Add files via upload

---
+++ CAT2_Q1.c
@@ -0,0 +1,40 @@
+//cat; HOW TO PROGRAM STRUCTURE
+//CAT2: structure program
+/*
+Author:Denzel Gitonga Mugendi
+Reg No:CT101/G/24518/24
+Date:7/11/2024
+*/
+#include <stdio.h>
+#include <string.h>
+
+struct{
+	char name[25];
+	int id;
+	char department[20];
+	float salary;
+	char email[50];
+} employee;
+
+int main(){
+	
+	strcpy(employee.name,"John Doe");
+	strcpy(employee.department,"Human Resources");
+	strcpy(employee.email,"john.doe@company.com");
+	employee.id=12345;
+	employee.salary=55000.50;
+	
+	
+	
+	printf("Name:%s\n",employee.name);
+	printf("ID:%d\n",employee.id);
+	printf("Department:%s\n",employee.department);
+	printf("Salary:%.2f\n",employee.salary);
+	printf("Email:%s\n",employee.email);
+	
+	
+	
+	
+	
+		return 0;
+}
